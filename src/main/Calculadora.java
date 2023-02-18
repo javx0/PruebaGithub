ab8639aea32ccbb70fa9444d80101b3cc21132aa
Javi <javierbailon2004@gmail.com>
2023-02-18 17:34:54 +0100

    Segundo commit

--- src/main/Calculadora.java
+++ src/main/Calculadora.java
@@ -13,10 +13,10 @@ public class Calculadora {
         
     }
     
-    public int suma(int num1,int num2){
+    /*public int suma(int num1,int num2){
         return num1+num2;
     }
-    
+    */
     public int resta(int num1,int num2){
         return num1-num2;
     }
